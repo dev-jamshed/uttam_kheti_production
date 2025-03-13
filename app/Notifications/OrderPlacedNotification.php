e613726a3e72e38be347a95d68c3d448212f5df8
Jamshed <119694561+dev-jamshed@users.noreply.github.com>
2025-03-13 11:54:46 +0500

    Update OrderPlacedNotification.php

--- app/Notifications/OrderPlacedNotification.php
+++ app/Notifications/OrderPlacedNotification.php
@@ -24,13 +24,14 @@ class OrderPlacedNotification extends Notification implements ShouldQueue
         return ['mail'];
     }
 
-    public function toMail($notifiable)
+   public function toMail($notifiable)
     {
         $array['subject'] = localize('Your order has been placed') . ' - ' . $this->order->orderGroup->order_code;
         $array['order'] = $this->order;
+        $font_family = env('INVOICE_FONT');
 
         return (new MailMessage)
-            ->view('backend.pages.orders.invoice', ['order' => $this->order])
+            ->view('backend.pages.orders.invoice', ['order' => $this->order, 'font_family' => $font_family])
             ->from(env('MAIL_FROM_ADDRESS'))
             ->subject(localize('Order Placed') . ' - ' . env('APP_NAME'));
     }
